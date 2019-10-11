1d0c0865f24db40213a41ca2b7fdd04c3c0722ad
neiljaitly7963 <neiljaitly7963@gmail.com>
2019-10-11 16:51:33 +0530

    weather app temp and weather working

---
+++ App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { StyleSheet, Text, View, Animated } from "react-native";
+import Weather from "./components/Weather";
+import { API_KEY } from "./utils/WeatherAPIKey";
+export default class App extends React.Component {
+  state = {
+    isLoading: false,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: "Error Gettig Weather Condtions"
+        });
+      }
+    );
+  }
+
+  fetchWeather(lat, lon) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          temperature: json.main.temp,
+          weatherCondition: json.weather[0].main,
+          isLoading: false
+        });
+      });
+  }
+
+  render() {
+    const { isLoading, weatherCondition, temperature } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoading ? (
+          <View style={styles.loadingContainer}>
+            <Text style={styles.loadingText}>Fetching The Weather</Text>
+          </View>
+        ) : (
+          <Weather weather={weatherCondition} temperature={temperature} />
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
---
+++ utils/WeatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = "b2fe80e84035f71b8311934173ed66f2";
